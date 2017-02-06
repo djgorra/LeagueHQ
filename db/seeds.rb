1d8c717e1dd927256514405186c169e146f85c20
Don Gorra <djgorra93@gmail.com>
2017-02-06 00:19:25 -0500

    Fixed seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,5 @@ response = HTTParty.get(url)
 response["data"].each do |champ|
   Champion.create!(name: champ[1]["name"], key: champ[1]["key"], riot_id: champ[1]["id"], title: champ[1]["title"], lore: champ[1]["lore"], img: champ[1]["image"]["full"])
 end
+
+#fix pls
