02f979e1d400f90cc098baa5ca29b479290ef965
Don Gorra <djgorra93@gmail.com>
2017-04-12 19:44:15 -0400

    Added victory/defeat

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -3,14 +3,22 @@ class MatchesController < ApplicationController
   def show
     url = "https://na.api.pvp.net/api/lol/na/v2.2/match/#{params[:id]}?api_key=#{ENV["api-key"]}"
     response = HTTParty.get(url)
-
+    @winners = []
+    @losers = []
     response["participants"].each do |participant|
       @champion = Champion.find_by(riot_id: participant["championId"])
       participant["championId"] = {name: @champion.name, key: @champion.key}
       participant["participantId"] = response["participantIdentities"][participant["participantId"]-1]["player"]
+      if participant["stats"]["winner"]
+        @winners << participant
+      else
+        @losers << participant
+      end
     end
+    response.delete("participants")
+    response.delete("participantIdentities")
 
-    render json: response
+    render :json => {:data => response, :winners => @winners, :losers => @losers}
   end
 
 end
--- react/src/components/MatchInfo.js
+++ react/src/components/MatchInfo.js
@@ -9,7 +9,7 @@ class MatchInfo extends Component {
   }
 
   render(){
-    let participants = this.props.data.participants.map(function(player){
+    let winners = this.props.data.winners.map(function(player){
 
       let divStyle = {
         backgroundImage: `url(http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${player.championId.key}_0.jpg)`,
@@ -20,7 +20,49 @@ class MatchInfo extends Component {
         <div className="media-object" style={divStyle}>
 
           <div className="media-object-section small-4">
-          <img height="50" width="50" src={"http://ddragon.leagueoflegends.com/cdn/7.6.1/img/profileicon/" + player.participantId.profileIcon + ".png"} />
+            <img height="50" width="50" src={"http://ddragon.leagueoflegends.com/cdn/7.6.1/img/profileicon/" + player.participantId.profileIcon + ".png"} />
+            <h5>{player.participantId.summonerName}</h5>
+            <p>{player.stats.kills}/{player.stats.deaths}/{player.stats.assists}</p>
+            <p>Level {player.stats.champLevel}</p>
+            <p>Minions killed: {player.stats.minionsKilled}</p>
+          </div>
+
+          <div className="media-object-section small-5">
+            <ul className="item-list">
+              <li><img height="25" width="25" src={`http://ddragon.leagueoflegends.com/cdn/7.6.1/img/item/${player.stats.item0}.png`} /></li>
+              <li><img height="25" width="25" src={`http://ddragon.leagueoflegends.com/cdn/7.6.1/img/item/${player.stats.item1}.png`} /></li>
+              <li><img height="25" width="25" src={`http://ddragon.leagueoflegends.com/cdn/7.6.1/img/item/${player.stats.item2}.png`} /></li>
+              <li><img height="25" width="25" src={`http://ddragon.leagueoflegends.com/cdn/7.6.1/img/item/${player.stats.item3}.png`} /></li>
+              <li><img height="25" width="25" src={`http://ddragon.leagueoflegends.com/cdn/7.6.1/img/item/${player.stats.item4}.png`} /></li>
+              <li><img height="25" width="25" src={`http://ddragon.leagueoflegends.com/cdn/7.6.1/img/item/${player.stats.item5}.png`} /></li>
+              <li><img height="25" width="25" src={`http://ddragon.leagueoflegends.com/cdn/7.6.1/img/item/${player.stats.item6}.png`} /></li>
+            </ul><br/>
+            <p>Towers destroyed: {player.stats.towerKills}</p>
+            <p>Best multikill: {player.stats.largestMultiKill}</p>
+            <p>Wards placed: {player.stats.wardsPlaced}</p>
+          </div>
+
+          <div className="media-object-section small-3">
+            <p>Damage dealt: {player.stats.totalDamageDealt}</p>
+            <p>Damage taken: {player.stats.totalDamageTaken}</p>
+            <p>Healing done: {player.stats.totalHeal}</p>
+          </div>
+        </div>
+      </li>;
+    })
+
+    let losers = this.props.data.losers.map(function(player){
+
+      let divStyle = {
+        backgroundImage: `url(http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${player.championId.key}_0.jpg)`,
+        backgroundSize: 'cover'
+      }
+
+      return <li key={player.participantId.summonerId} className="winners">
+        <div className="media-object" style={divStyle}>
+
+          <div className="media-object-section small-4">
+            <img height="50" width="50" src={"http://ddragon.leagueoflegends.com/cdn/7.6.1/img/profileicon/" + player.participantId.profileIcon + ".png"} />
             <h5>{player.participantId.summonerName}</h5>
             <p>{player.stats.kills}/{player.stats.deaths}/{player.stats.assists}</p>
             <p>Level {player.stats.champLevel}</p>
@@ -55,7 +97,9 @@ class MatchInfo extends Component {
       <div className="callout">
         <ul>
           <h3>VICTORY</h3>
-          {participants}
+          {winners}
+          <h3>DEFEAT</h3>
+          {losers}
         </ul>
       </div>
     );
